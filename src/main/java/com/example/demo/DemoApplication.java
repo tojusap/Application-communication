f28be5136b478023fa4f36b615e7079f5a16af57
FWdarling <1853204@tongji.edu.cn>
2020-12-17 04:25:18 +0800

    use rabbit message queue

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,47 @@
 package com.example.demo;
 
+import com.example.demo.rabbitmq.Receiver;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
+
+	static final String topicExchangeName = "test-topic-exchange";
+
+	static final String queueName = "test-queue";
+
+	@Bean
+	Queue queue(){
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange(){
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+											 MessageListenerAdapter listenerAdapter){
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/com/example/demo/rabbitmq/Receiver.java
@@ -0,0 +1,25 @@
+package com.example.demo.rabbitmq;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * description: Receiver
+ * date: 12/17/20 3:32 AM
+ * author: fourwood
+ */
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessagge(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
